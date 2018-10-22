bc8a91c1a9eb05acf1fb93fd30e61f14b5252b08
WooxSolo <twoxsolo123@gmail.com>
2018-10-23 01:52:55 +0200

    Use colons instead of commas for WorldPoint config

--- runelite-client/src/main/java/net/runelite/client/config/ConfigManager.java
+++ runelite-client/src/main/java/net/runelite/client/config/ConfigManager.java
@@ -525,7 +525,7 @@ public class ConfigManager
 		}
 		if (type == WorldPoint.class)
 		{
-			String[] splitStr = str.split(",");
+			String[] splitStr = str.split(":");
 			int x = Integer.parseInt(splitStr[0]);
 			int y = Integer.parseInt(splitStr[1]);
 			int plane = Integer.parseInt(splitStr[2]);
@@ -575,7 +575,7 @@ public class ConfigManager
 		if (object instanceof WorldPoint)
 		{
 			WorldPoint wp = (WorldPoint)object;
-			return wp.getX() + "," + wp.getY() + "," + wp.getPlane();
+			return wp.getX() + ":" + wp.getY() + ":" + wp.getPlane();
 		}
 		if (object instanceof Duration)
 		{
